027ca530436005925233f5d416f5d4cad4a4562f
”20_Suzuki_Masahito” <uppo1182@gmail.com>
2019-07-12 08:21:38 +0000

    first commit

---
+++ practice01.php
@@ -0,0 +1,18 @@
+<?php
+$name = "鈴木政仁";
+if ($name == "鈴木政仁") {
+    echo "私は".$name."です";
+}
+else {
+    echo $name."ではありません";
+}
+
+/* 条件が $name != "あなたの名前"の場合
+$name = "鈴木政仁";
+if ($name !="鈴木政仁") {
+    echo $name."ではありません";
+}
+else {
+    echo "私は".$name."です";
+}
+*/
---
+++ practice03.php
@@ -0,0 +1,6 @@
+<?php
+$fruits = array("桃","梨","ぶどう","メロン","みかん");
+foreach($fruits as $fruit) {
+    echo "好きなフルーツは".$fruit."です";
+    echo "\n";
+}
